f298a1b701175b1c535c324af81503acfb98650c
cindrmon <feroxafarensis019@protonmail.com>
2020-09-27 14:01:11 +0800

    modify fish config: make new fish greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,11 +17,7 @@ set -gx PATH $PATH $HOME/.config/polybar
 ## Custom Fish Greeting
 
 function fish_greeting
-    echo "                  __                                 __            ____ _        __  "
-    echo " _      __ ___   / /_____ ____   ____ ___   ___     / /_ ____     / __/(_)_____ / /_ "
-    echo "| | /| / // _ \ / // ___// __ \ / __ \__ \ / _ \   / __// __ \   / /_ / // ___// __ "
-    echo "| |/ |/ //  __// // /__ / /_/ // / / / / //  __/  / /_ / /_/ /  / __// /(__  )/ / / /"
-    echo "|__/|__/ \___//_/ \___/ \____//_/ /_/ /_/ \___/   \__/ \____/  /_/  /_//____//_/ /_/ "
-    echo "                                                                                     "
+    echo "Welcome to NEXVS-ALPHA!";
+    echo "To know more about a specific command, type '[man/tldr] <command>'";
 end
 funcsave fish_greeting
