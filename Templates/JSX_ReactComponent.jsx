bc44d95a0b141cf2389d10f309eb91eccdb01ce3
cindrmon <gdfrancia01901@protonmail.ch>
2021-05-12 16:29:26 +0800

    update Template Files

---
+++ Templates/JSX_ReactComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+// import components here
+
+// insert data variables here
+
+// insert component methods here
+
+// insert computed values here
+
+// template
+function ReactComponent() {
+
+	return(
+	<div id="reactComponent">
+		<h1>React Component Output Code</h1>
+	</div>
+	);
+
+}
+
+export default ReactComponent;
